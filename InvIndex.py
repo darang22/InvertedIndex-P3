f7a1186dc14c8ebc8b289775e70d25446e6ccf12
Daniel Arango <skap.mks@gmail.com>
2017-04-26 15:33:34 -0500

    Se modifica el regex y se limpian las palabras de caracteres especiales

--- InvIndex.py
+++ InvIndex.py
@@ -1,9 +1,18 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+from mrjob.protocol import TextValueProtocol
 import re
 import os
 
-WORD_RE = re.compile(r"[\w']+")
+import unicodedata
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+# Optional apostrophe only between characters (w)+ ('? w)+
+WORD_RE = re.compile(r"[^\W_]+[\S]*[^\W_]+", re.UNICODE)
+
 class MRWordFrequencyCount(MRJob):
 
   def getKey(item):
@@ -12,7 +21,7 @@ class MRWordFrequencyCount(MRJob):
   def mapper(self, _, line):
     for word in WORD_RE.findall(line):
       fileName = os.environ['mapreduce_map_input_file']
-      yield (word,fileName), 1
+      yield (word.lower(),fileName), 1
 
   def reducer(self, fileName, rep):
     yield fileName[0], (fileName[1], sum(rep))
@@ -21,7 +30,9 @@ class MRWordFrequencyCount(MRJob):
     l = list(values)
     #Returns the top 10 documents where the key word is repeated the most
     l = sorted(l, key=lambda x: x[1], reverse = True)[:10]
-    yield word, str(l)
+    #Remove special characters
+    output = unicodedata.normalize('NFD', word).encode('ascii','ignore')  
+    yield output, str(l)
 
   def steps(self):
     return [
@@ -30,116 +41,3 @@ class MRWordFrequencyCount(MRJob):
 if __name__ == '__main__':
     MRWordFrequencyCount.run()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-""""
-
-WORD_RE = re.compile(r"[\w']+")
-class MRWordFrequencyCount(MRJob):
-  def mapper(self, _, line):
-    for word in WORD_RE.findall(line):
-      fileName = os.environ['mapreduce_map_input_file']
-      yield word,fileName
-
-  def reducer(self, word, fileName):
-      l = list(set(fileName))
-      yield word,l
-
-
-WORD_RE = re.compile(r"[\w']+")
-class MRWordFrequencyCount(MRJob):
-  def mapper(self, _, line):
-    for word in WORD_RE.findall(line):
-      fileName = os.environ['mapreduce_map_input_file']
-      yield fileName,1
-
-  def reducer(self, fileName, counts):
-      yield fileName,sum(counts)
-
-if __name__ == '__main__':
-    MRWordFrequencyCount.run()
-
-
-from mrjob.job import MRJob
-
-class MRWordFrequencyCount(MRJob):
-
-    def mapper(self, _, line):
-	se, ide, salario, year  = line.decode('utf-8','ignore').split()
-        yield se, int(salario)
-
-    def reducer(self, se, salario):
-      l = list(salario)
-      yield se, sum(l)/len(l)
-
-
-
-if __name__ == '__main__':
-    MRWordFrequencyCount.run()
-
-
-from mrjob.job import MRJob
-
-class MRWordFrequencyCount(MRJob):
-
-    def mapper(self, _, line):
-  se, ide, salario, year  = line.decode('utf-8','ignore').split()
-        yield ide, int(salario)
-
-    def reducer(self, ide, salario):
-      l = list(salario)
-      yield ide, sum(l)/len(l)
-
-
-
-if __name__ == '__main__':
-    MRWordFrequencyCount.run()
-
-
-
-from mrjob.job import MRJob
-
-class MRWordFrequencyCount(MRJob):
-
-    def mapper(self, _, line):
-  se, ide, salario, year  = line.decode('utf-8','ignore').split()
-        yield se, ide
-
-    def reducer(self, se, ide):
-      l = list(ide)
-      yield se, len(l)
-
-
-
-if __name__ == '__main__':
-    MRWordFrequencyCount.run()
-
-"""
